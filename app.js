8409b3c68d6537d0ee485a6d66e870680143b2ac
Alex White <awhit012@gmail.com>
2016-02-13 16:15:42 -0800

    final commit

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ App.prototype.addEventListener = function(){
   var app = this;
   this.$button.on("click", function(event){
     event.preventDefault();
+    app.getSearchInput();
     app.searchImdb();
   });
 };
@@ -21,6 +22,29 @@ App.prototype.addEventListener = function(){
 App.prototype.searchImdb = function(){
   var app = this;
   $.ajax({
-    url: app.url + "Big Lebowski"
-  }).success(function(data){console.log(data)});
+    url: app.url + app.formInput
+  }).success( function(data){
+    app.checkResponse(data);
+  });
+};
+
+App.prototype.displayPoster = function(posterUrl){
+  var image = "<img src=" + posterUrl + ">"
+  this.$posterDiv.html(image);
+};
+
+App.prototype.getSearchInput = function() {
+  this.formInput = this.$input.val()
+}
+
+App.prototype.showError = function(){
+  console.log("in showError")
+  this.$posterDiv.html("No poster available");
+}
+
+
+App.prototype.checkResponse = function(data){
+  if(data["Poster"]){
+    app.displayPoster(data["Poster"]);
+  } else { app.showError() };
 }
